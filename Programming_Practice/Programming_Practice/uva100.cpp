54550fe446d9aa2b9e41afce3489ab4f5cac104f
BoboMJZhou <zhou970610@csie.ndhu.edu.tw>
2019-05-28 00:31:02 +0800

    The 3n+1 problem
    uva100, didnt get result yet

--- Programming_Practice/Programming_Practice/uva100.cpp
+++ Programming_Practice/Programming_Practice/uva100.cpp
@@ -2,12 +2,42 @@
 #include <iostream>
 using namespace std;
 
+int FindCycleLength(int x)
+{
+	int length = 1;
+	if (x == 1)
+	{
+		return 1;
+	}
+	else if (x % 2 == 0)
+	{
+		return length + FindCycleLength(x / 2);
+	}
+	else
+	{
+		return length + FindCycleLength( x * 3 + 1);
+	}
+}
+
 int main()
 {
 	int i, j;
 	while (cin >> i >> j)
 	{
-		cout << i << j << endl;
+		if (i > j)
+		{
+			int t = i;
+			i = j;
+			j = t;
+		}
+		int max_cycle = 0;
+		for (int x = i; x <= j; x++)
+		{
+			int temp_cycle = FindCycleLength(x);
+			if (max_cycle < temp_cycle)
+				max_cycle = temp_cycle;
+		}
+		cout << i << " " << j << " " << max_cycle << endl;
 	}
 	return 0;
 }
